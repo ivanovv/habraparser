9e9d7f101548513cb58b3d7d1cf6e164e6c0d0f2
blackfoks <mail@blackfoks.com>
2011-07-26 00:45:01 +0700

    Habr.user_agent spec described

--- spec/habr_spec.rb
+++ spec/habr_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe Habr do
-  it "should have default user agent"
+  
+  describe ".user_agent" do
+    
+    it "should be 'Mozilla/5.0' by default" do
+      Habr.user_agent.should == 'Mozilla/5.0'
+    end
+    
+  end
+  
   it "should open a habrapage"
+  
 end

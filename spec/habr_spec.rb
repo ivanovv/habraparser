34bde144dc6494a7f590c46151fa4df40bc0e2a6
blackfoks <mail@blackfoks.com>
2011-07-29 01:58:46 +0700

    user_agent changed

--- spec/habr_spec.rb
+++ spec/habr_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Habr do
   
   its(:user_agent) { should_not be_nil }
-  its(:user_agent) { should == 'Mozilla/5.0' }
+  its(:user_agent) { should == "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0.1) Gecko/20100101 Firefox/5.0.1" }
   
   # it "user_agent should be changable"
   

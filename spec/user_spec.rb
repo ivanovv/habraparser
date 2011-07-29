a21c367ebc015788d306e1eaf5dd833a4786d406
blackfoks <mail@blackfoks.com>
2011-07-29 18:30:55 +0700

    blacfoks added into user_spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe Habr::User do
+  # alizar
   let(:alizar) { Habr::User.find 'alizar' }
   specify { alizar.name.should == 'alizar'} 
-  specify { alizar.favs_count.should > 0 }  
+  specify { alizar.favs_count.should > 0 }
+  
+  # blackfoks
+  let(:blackfoks) { Habr::User.find 'blackfoks' }
+  specify { blackfoks.name.should == 'BlackFoks' }
+  specify { blackfoks.favs_count.should >= 770 }
 end

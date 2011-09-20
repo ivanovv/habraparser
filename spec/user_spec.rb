35b19eaf450f7bf49a60522e6c457dcbfb19504c
blackfoks <mail@blackfoks.com>
2011-09-20 10:58:21 +0700

    User change

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,13 +1,9 @@
 require 'spec_helper'
 
 describe Habr::User do
-  # alizar
-  let(:alizar) { Habr::User.find 'alizar' }
-  specify { alizar.name.should == 'alizar'} 
-  specify { alizar.favs_count.should > 0 }
-  
-  # blackfoks
-  let(:blackfoks) { Habr::User.find 'blackfoks' }
-  specify { blackfoks.name.should == 'BlackFoks' }
-  specify { blackfoks.favs_count.should >= 770 }
+  subject { Habr::User.find 'blackfoks' }
+
+  its(:slug) { should == 'blackfoks'}
+  its(:name) { should == 'BlackFoks'}
+  its(:favs) { should_not be_nil }
 end

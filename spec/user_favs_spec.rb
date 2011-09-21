31961fef430dfe638b4cea2560c554fb299bcc03
blackfoks <mail@blackfoks.com>
2011-09-21 10:20:59 +0700

    Specs change

--- spec/user_favs_spec.rb
+++ spec/user_favs_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe Habr::UserFavs do
   subject { Habr::UserFavs.new(:userslug => 'alizar') }
 
-  it { should have_at_least(2).pages }
+  it { should have_at_least(4).pages }
   it { should have_at_most(90).pages }
   it { should respond_to(:each) }
 
-  its(:count) { should > 0 }
+  its(:count) { should >= 33 }
 
   it "should accept :userslug" do
     favs = Habr::UserFavs.new(:userslug => 'blackfoks')

42461c5ce87f1581a643ad58be21953a946679f7
blackfoks <mail@blackfoks.com>
2011-09-21 10:58:35 +0700

    lib structure change, autoload replaced by require

--- lib/habraparser.rb
+++ lib/habraparser.rb
@@ -1,24 +1,23 @@
 require 'nokogiri'
 require 'open-uri'
 
+require "habraparser/user"
+require "habraparser/fav"
+require "habraparser/user_favs"
+require "habraparser/favs_page"
+require "habraparser/links"
+require "habraparser/helper"
+
+
 module Habr
-  
-  autoload :User, 'user.rb'
-  autoload :Fav, 'fav.rb'
-  autoload :UserFavs, 'user_favs.rb'
-  autoload :FavsPage, 'favs_page.rb'
-  autoload :Links, 'links.rb'
-  autoload :Helper, 'helper.rb'
-  
   # Returns default user agent
   def self.user_agent
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0.1) Gecko/20100101 Firefox/5.0.1"
   end
-  
+
   # Opens page on habrahabr.ru with default user agent.
   # Please, use this method instead of direct nokogiri.
   def self.open_page(habraurl)
     Nokogiri::HTML(open(habraurl, "User-Agent" => self.user_agent))
   end
-  
 end

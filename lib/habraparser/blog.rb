4408ac3bb9e8a24da5b0a51ed65f4f11f0d69eb7
blackfoks <mail@blackfoks.com>
2011-09-23 22:20:55 +0700

    Now corporate blogs can be found
     * Added Blog#corporate? method.
     * Blog can recognize itself as corporate or shared.
     * Blog prefers to be shared instead of corporate.
     * Blog can correct incorrect :corporate key while loading blog's page.

--- lib/habraparser/blog.rb
+++ lib/habraparser/blog.rb
@@ -2,26 +2,60 @@ module Habr
   class Blog
     attr_reader :slug
 
-    def initialize(slug)
+    def initialize(slug, opts={})
       @slug = slug
+      @corporate = opts[:corporate]
     end
 
     def title
       @title ||= blog_link.text.strip
     end
 
-    def self.find(slug)
-      Blog.new slug
+    def corporate?
+      if @corporate.nil?
+        page
+      end
+      @corporate
+    end
+
+    def self.find(slug, opts={})
+      Blog.new slug, opts
     end
 
     protected
 
       def page
-        @page ||= Habr.open_page(Habr::Links.blog(@slug))
+        @page ||= get_page
+      end
+
+      def get_page
+        is_corporate = @corporate || false # it's shared by default
+
+        # try to use current #corporate? state
+        begin
+          new_page = Habr.open_page(Habr::Links.blog @slug, :corporate => is_corporate)
+        # it seems like current #corporate? state is wrong, so try to use inverse state
+        rescue Exception => e
+          new_page = Habr.open_page(Habr::Links.blog @slug, :corporate => !is_corporate)
+          @corporate = !is_corporate
+        end
+
+        new_page
       end
 
       def blog_link
-        @blog_link ||= page.css('.blog-header a').first
+        @blog_link ||= get_blog_link
+      end
+
+      def get_blog_link
+        shared_blog_link = page.css('.blog-header a').first
+        if shared_blog_link
+          @corporate = false
+          return shared_blog_link
+        else
+          @corporate = true
+          return page.css('.company-header .habrauser').first
+        end
       end
 
   end
--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require 'spec_helper'
 
 describe Habr::Blog do
@@ -7,5 +8,50 @@ describe Habr::Blog do
     it { should_not be_nil }
     its(:slug) { should == 'ror' }
     its(:title) { should == 'Ruby on Rails'}
+    its(:corporate?) { should be_false }
+  end
+
+  describe "corporate blog" do
+    subject { Habr::Blog.find 'mosigra', :corporate => true }
+
+    it { should_not be_nil }
+    its(:slug) { should == 'mosigra' }
+    its(:title) { should == 'Мосигра'}
+    its(:corporate?) { should be_true}
+  end
+
+  describe "shared/corporate recognition" do
+    describe "if no :corporate info given" do
+      it "should recognize corporate blog" do
+        blog = Habr::Blog.find 'mosigra'
+        blog.corporate?.should be_true
+      end
+
+      it "should recognize shared blog" do
+        blog = Habr::Blog.find 'ror'
+        blog.corporate?.should be_false
+      end
+
+      it "should prefer shared instead of corporate" do
+        blog = Habr::Blog.find 'yandex'
+        blog.corporate?.should be_false
+      end
+    end
+
+    describe "if wrong info given" do
+      it "should change shared to corporate" do
+        blog = Habr::Blog.find 'mosigra', :corporate => false
+        blog.corporate?.should be_false
+        blog.title # force loading page
+        blog.corporate?.should be_true
+      end
+
+      it "should chage corporate to shared" do
+        blog = Habr::Blog.find 'ror', :corporate => true
+        blog.corporate?.should be_true
+        blog.title # force loading page
+        blog.corporate?.should be_false
+      end
+    end
   end
 end

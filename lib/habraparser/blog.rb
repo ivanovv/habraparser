4e0e841ef5b7e7b41369cc38f0f9b62691595843
blackfoks <mail@blackfoks.com>
2011-09-26 14:31:27 +0700

    Blog refactor
    Added comments.

--- lib/habraparser/blog.rb
+++ lib/habraparser/blog.rb
@@ -1,16 +1,23 @@
 module Habr
+  # Represents habrablog.
   class Blog
     attr_reader :slug
 
+    # Creates a new blog with given slug. May have following options:
+    # * :corporate - is blog corporate or not (i.e. shared)
     def initialize(slug, opts={})
       @slug = slug
       @corporate = opts[:corporate]
     end
 
+    # Gets blog's title (like "Ruby on Rails"). Lazy method.
     def title
       @title ||= blog_link.text.strip
     end
 
+    # Is blog corporate or not (i.e. shared).
+    # If corresponding value has not set bafore (using #find, #new or #known) then 
+    # loads blog page to get it.
     def corporate?
       if @corporate.nil?
         page
@@ -18,6 +25,10 @@ module Habr
       @corporate
     end
 
+    # Sets some known information about this blog. Have options:
+    # * title - blog's title
+    # * corporate - is blog corporate or shared
+    # Returns self for method chaining.
     def known(opts={})
       @title ||= opts[:title]
       @corporate ||= opts[:corporate]
@@ -25,16 +36,21 @@ module Habr
       self
     end
 
+    # Returns new blog with given params but doesn't try to find any data on habrahabr 
+    # untill any method call.
     def self.find(slug, opts={})
       Blog.new slug, opts
     end
 
     protected
 
+      # Gets appropriate page. Lazy method.
       def page
         @page ||= get_page
       end
 
+      # Returns an appropriate page. If :corporate has not been set before then try to 
+      # analyse the page to find out whether the blog is corporate or shared.
       def get_page
         is_corporate = @corporate || false # it's shared by default
 
@@ -50,10 +66,13 @@ module Habr
         new_page
       end
 
+      # Gets Nokogiri::XmlNode for blog link on the page. Lazy method.
       def blog_link
         @blog_link ||= get_blog_link
       end
 
+      # Returns Nokogiri::XmlNode for blog link. Try no find out whether the blog is 
+      # corporate or shared.
       def get_blog_link
         shared_blog_link = page.css('.blog-header a').first
         if shared_blog_link
